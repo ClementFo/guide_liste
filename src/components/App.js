1deec492178daad692ca78693a56963374a7d875
clement <clementfornes@gmail.com>
2026-07-27 10:31:49 +0200

    Modification des components pour  optimisation

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import '../styles/App.css';
+import Guide from './Guide'
+
+function App({ GuideList }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          {GuideList.map((item, index) => (
+              <Guide
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  date={item.date}
+                  activité={item.activité}
+              />
+          ))}
+      </div>
   );
 }
 
