27c7ecbeb9c983961e91b553d54e4aa90f62045f
clement <clementfornes@gmail.com>
2026-07-27 10:31:01 +0200

    Ajout de guides pour les tests d'affichage

---
+++ src/datas/GuideList.js
@@ -0,0 +1,26 @@
+export const GuideList = [
+    {
+        title: "Guide 1",
+        description: "Description du guide 1",
+        date: "2023-01-01",
+        activité: "Activité 1"
+    },
+    {
+        title: "Guide 2",
+        description: "Description du guide 2",
+        date: "2023-01-02",
+        activité: "Activité 2"
+    },
+    {
+        title: "Guide 3",
+        description: "Description du guide 3",
+        date: "2023-01-03",
+        activité: "Activité 3"
+    },
+    {
+        title: "Guide 4",
+        description: "Description du guide 4",
+        date: "2023-01-04",
+        activité: "Activité 4"
+    }
+];
